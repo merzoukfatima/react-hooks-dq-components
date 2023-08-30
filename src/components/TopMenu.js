a8132d31eb27536d0a79e2155b4ab12041fe6017
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-08-30 19:58:42 +0100

    Assignment Completed

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function TopMenu() {
   return (
-    <nav className="menu">
-      <button>
-        <span className="profile-logo" role="img" aria-label="user">
-          👤
-        </span>
-        <span className="profile-menu">username</span>
-      </button>
-    </nav>
+      <nav className="menu">
+        <button>
+          <span className="profile-logo" role="img" aria-label="user">
+            👤
+          </span>
+          <span className="profile-menu">username</span>
+        </button>
+      </nav>
   );
 }
 
